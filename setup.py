28507aed257d1157f849e1c211bed518732759d6
Manuel Leuchtenmueller <leichti@gmail.com>
2025-02-03 10:04:54 +0100

    Bump version to 0.2.0 in setup.py; add sample repository test for data filtering and visualization

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fastTGA',
-    version='0.1.6',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[
         'PyQt6',
